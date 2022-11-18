b9e1f2d47815ebbff99c0d3d3100f2eb2a534e71
victortimsit <victortimsit.contact@gmail.com>
2022-11-18 14:25:53 +0100

    update 5 files and create 1 file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,90 @@
-import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
+import StartButton from "./components/StartButton";
 
-const OPEN_AI_TOKEN = "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";
+const OPEN_AI_TOKEN = "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";
+const initialMessage = { speaker: "Human", message: "Play to start talking" };
+const initialConversationHistory = [
+  "Human: Hello, who are you?",
+  "AI: I am Watson. How can I help you today?",
+];
 
 export default function Home() {
   const SpeechRecognition = useRef<any>(null);
   const SpeechGrammarList = useRef<any>(null);
   const SpeechRecognitionEvent = useRef<any>(null);
   const SpeechSynthesis = useRef<any>(null);
+  const recognition = useRef<any>(null);
+  const speechRecognitionList = useRef<any>(null);
+  const syntesis = useRef<any>(null);
+
   const [talking, setTalking] = useState(false);
   // const started = useRef(false);
+  const [started, setStarted] = useState(false);
   const [messages, setMessages] = useState<
     Array<{ speaker: string; message: string }>
-  >([{ speaker: "Human", message: "Say: Hey Watson" }]);
-  const conversationHistory = useRef<Array<string>>([
-    "Human: Hello, who are you?",
-    "AI: I am an AI created by OpenAI. How can I help you today?",
-  ]);
+  >([initialMessage]);
+  const conversationHistory = useRef<Array<string>>(initialConversationHistory);
   const [result, setResult] = useState("");
 
   const startSpeechToText = () => {
     console.log("Start");
+    setStarted(true);
 
-    const recognition = new SpeechRecognition.current();
-    const speechRecognitionList = new SpeechGrammarList.current();
+    recognition.current = new SpeechRecognition.current();
+    speechRecognitionList.current = new SpeechGrammarList.current();
 
-    recognition.start();
+    recognition.current.start();
 
-    recognition.onresult = (event: any) => {
+    recognition.current.onresult = (event: any) => {
       const text = event.results[0][0].transcript;
       console.log(text);
-      // if (text.toLowerCase().includes("watson")) started.current = true;
-      // if (started.current == false) return;
+
       setResult(text);
       handleAnswer(text);
     };
-    recognition.onspeechstart = () => {
+    recognition.current.onspeechstart = () => {
       setTalking(true);
     };
 
-    recognition.onspeechend = () => {
-      setTalking(false);
-      recognition.stop();
-      // if (started.current == false) startSpeechToText();
-    };
+    recognition.current.onspeechend = stopSpeechToText;
+  };
+
+  const stopSpeechToText = () => {
+    setTalking(false);
+    recognition.current.stop();
   };
 
   const startTextToSpeech = (message: string) => {
     setTalking(true);
     const utterThis = new SpeechSynthesisUtterance(message);
-    const syntesis = SpeechSynthesis.current;
+    syntesis.current = SpeechSynthesis.current;
 
-    syntesis.speak(utterThis);
+    syntesis.current.speak(utterThis);
 
     utterThis.onend = () => {
-      setTalking(false);
+      console.log("END");
+      stopTextToSpeech();
       startSpeechToText();
     };
+  };
+
+  const stopTextToSpeech = () => {
+    setTalking(false);
+    if (syntesis.current) syntesis.current.cancel();
+  };
+
+  const start = () => {
+    handleAnswer("Ask me a question to start the conversation");
+  };
+
+  const restart = () => {
+    console.log("restart");
+    stopSpeechToText();
+    stopTextToSpeech();
+
+    conversationHistory.current = initialConversationHistory;
+    setMessages([initialMessage]);
+    setStarted(false);
   };
 
   const handleAnswer = async (message: string) => {
@@ -64,6 +92,7 @@ export default function Home() {
     setMessages((m) => [...m, { speaker: "Human", message }]);
     const res = await getGPT3Answer(message);
     const answer: string = res.choices[0].text;
+    // const answer = "Hello";
     const AIAnswer = `AI: ${answer}`;
     if (conversationHistory.current.length > 8)
       conversationHistory.current.splice(0, 2);
@@ -76,11 +105,7 @@ export default function Home() {
 
   const getGPT3Answer = async (message: string) => {
     console.log("HISTORY ", conversationHistory);
-    console.log(
-      `The following is a conversation with an AI assistant that help to practice English. His name is Watson. The assistant is helpful, creative, clever, curious and very friendly. The assistant asks a lot of questions and likes to talk and likes to initiate conversation.\n${conversationHistory.current.join(
-        "\n"
-      )}\nHuman:${message}\n\nAI:`
-    );
+
     return await fetch("https://api.openai.com/v1/completions", {
       method: "POST",
       headers: {
@@ -89,11 +114,13 @@ export default function Home() {
       },
       body: JSON.stringify({
         model: "text-davinci-002",
-        prompt: `The following is a conversation with an AI assistant that help to practice English. The assistant is helpful, creative, clever, and very friendly. The assistant signal every mistake in the Human last message and correct to standard English. The assistant asking questions.\n${conversationHistory.current.join(
+        prompt: `The following is a conversation with an AI assistant that help to practice English. The assistant uses open questions and keep the conversation going. The assistant don't talks about the same subject more than twice. \n${conversationHistory.current.join(
           "\n"
         )}\nHuman:${message}\n\nAI:`,
         temperature: 1,
         max_tokens: 100,
+        presence_penalty: 2,
+        frequency_penalty: 2,
       }),
     }).then((r) => r.json());
   };
@@ -114,49 +141,38 @@ export default function Home() {
   useEffect(() => {
     initState();
     // startSpeechToText();
-    window.addEventListener("click", startSpeechToText);
+    // window.addEventListener("click", startSpeechToText);
 
-    return () => window.removeEventListener("click", startSpeechToText);
+    // return () => window.removeEventListener("click", startSpeechToText);
   }, []);
 
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* <div className="w-[200px] h-[200px] bg-pink-400 absolute blur-[200px] opacity-80" /> */}
       <div
-        className={`w-[300px] h-[300px] bg-gradient-to-tr from-pink-400 to-blue-400 absolute blur-[200px] opacity-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all ${
-          talking ? "animate-pulse" : ""
+        className={`w-[300px] h-[300px] animate-pulse bg-gradient-to-tr from-pink-400 to-blue-400 absolute blur-[200px] opacity-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all pointer-events-none ${
+          talking ? "play" : "pause"
         }`}
       />
-      {/* <div className="w-[200px] h-[200px] bg-blue-400 absolute blur-[200px] opacity-80 right-32 bottom-32" /> */}
+
       <main
-        className={`flex flex-col items-center justify-center h-screen ${
+        className={`flex flex-col items-center justify-center h-screen px-8 ${
           messages[messages.length - 1].speaker == "Human" ? "opacity-50" : ""
         }`}
       >
         <div className="flex gap-2 items-center">
           <div
-            className={`transition-all w-0.5 h-5 rounded-full bg-white ${
-              talking ? "scale-y-1 animate-breave" : "scale-y-[.1]"
+            className={`transition-all animate-breave animation-delay-100 w-0.5 h-5 rounded-full bg-white ${
+              talking ? "scale-y-1 play" : "scale-y-[.1] pause"
             }`}
           />
           <div
-            className={`transition-all w-0.5 h-10 rounded-full bg-white ${
-              talking
-                ? "scale-y-1 animate-breave animation-delay-100"
-                : "scale-y-[.05]"
+            className={`transition-all animate-breave animation-delay-200 w-0.5 h-10 rounded-full bg-white ${
+              talking ? "scale-y-1 play" : "scale-y-[.05] pause"
             }`}
           />
           <div
-            className={`transition-all w-0.5 h-5 rounded-full bg-white ${
-              talking
-                ? "scale-y-1 animate-breave animation-delay-200"
-                : "scale-y-[.1]"
+            className={`transition-all animate-breave animation-delay-300 w-0.5 h-5 rounded-full bg-white ${
+              talking ? "scale-y-1 play" : "scale-y-[.1] pause"
             }`}
           />
         </div>
@@ -169,6 +185,12 @@ export default function Home() {
         >
           {messages[messages.length - 1].message}
         </span>
+        <StartButton
+          className="mt-8"
+          started={started}
+          onStart={start}
+          onRestart={restart}
+        />
       </main>
     </div>
   );
