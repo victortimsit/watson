dae8bb780e9daeccb96faaef72520d04cdceb298
victortimsit <victortimsit.contact@gmail.com>
2022-11-21 14:48:08 +0100

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import "../styles/globals.css";
+import { DynamicHead } from "./components/DynamicHead";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Watson</title>
-        <meta
-          name="description"
-          content="Improve your English speaking skills by talking to Watson"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <script async src="https://cdn.splitbee.io/sb.js" />
-      </Head>
+      <DynamicHead />
       <Component {...pageProps} />
     </>
   );
